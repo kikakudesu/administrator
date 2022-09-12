457c6ec8d601bc9b5d99fc48563c2aad402dd20a
kikakudesu <112141878+kikakudesu@users.noreply.github.com>
2022-09-12 14:39:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const app = express()
 
 app.use(express.static(path.join(__dirname,"dist")))
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
   console.log("I'm running");
